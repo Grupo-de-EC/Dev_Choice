1df83a5838a7f70317a39293c4bcc22074685495
MatheusArruda2005 <163347306+MatheusArruda2005@users.noreply.github.com>
2025-04-04 11:11:52 -0300

    Add files via upload

---
+++ Formulario.js
@@ -0,0 +1,4 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita o recarregamento da página
+    window.location.href = "index.html"; // Redireciona para a página index.html
+  });
