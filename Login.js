3a7543e1e328f243ce008e4dd297cd6c64a57c11
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2025-04-04 10:10:37 -0300

    ...........

--- Login.js
+++ Login.js
@@ -1,4 +1,9 @@
 document.getElementById("loginButton").addEventListener("click", function () {
-  const username = document.getElementById("username").value;
-  localStorage.setItem("nomeUsuario", username);
+  const username = document.getElementById("username").value.trim();
+
+  if (username) {
+    localStorage.setItem("nomeUsuario", username);
+  } else {
+    alert("Por favor, insira seu nome.");
+  }
 });
