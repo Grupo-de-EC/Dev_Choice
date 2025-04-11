0edab806b2cd7de0acbe560fdfb4ff9623d6028e
João Vitor de Moraes Bringmann <149213160+JoaoBringmann@users.noreply.github.com>
2025-04-11 10:53:49 -0300

    Agora funciona o contraste em tudo

--- index.js
+++ index.js
@@ -21,32 +21,18 @@ document.getElementById("contrasteBtn").addEventListener("click", () => {
   document.querySelector("#input-area").classList.toggle("alto-contraste");
 });
 
-const nome = localStorage.getItem("nomeUsuario");
-console.log("Nome recuperado do localStorage:", nome);
-if (nome) {
-  document.getElementById("boasVindas").textContent = `Bem-vindo, ${nome}!`;
-}
-
-  //Nome do projeto em cima da pagina do index
 window.addEventListener("DOMContentLoaded", function () {
+  const nome = localStorage.getItem("nomeUsuario");
   const projeto = localStorage.getItem("tipoProjeto");
-  if (projeto) {
-    const boasVindas = document.getElementById("boasVindas");
-    const spanProjeto = document.createElement("span");
-    spanProjeto.textContent = projeto;
-    spanProjeto.style.color = "#007bff";
-    spanProjeto.style.fontWeight = "bold";
-    spanProjeto.style.textTransform = "capitalize";
 
-    const paragrafo = document.createElement("p");
-    paragrafo.appendChild(spanProjeto);
-    paragrafo.style.textAlign = "center";
-    paragrafo.style.marginTop = "10px";
-    paragrafo.style.fontSize = "2rem";
-    paragrafo.style.fontWeight = "bold";
-    paragrafo.style.fontFamily = "Arial, sans-serif";
-    paragrafo.style.textShadow = "2px 2px 5px rgba(0, 123, 255, 0.5)";
+  const boasVindas = document.getElementById("boasVindas");
 
-    boasVindas.insertAdjacentElement("afterend", paragrafo);
+  if (nome && projeto) {
+    boasVindas.innerHTML = `Bem-vindo, ${nome},${projeto}!`;
+  } else if (nome) {
+    boasVindas.textContent = `Bem-vindo, ${nome}!`;
+  } else if (projeto) {
+    boasVindas.textContent = `Bem-vindo ao projeto ${projeto}!`;
   }
 });
+
