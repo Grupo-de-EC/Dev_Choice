e6efc01eb4eab97c035ac00f359c47dbd4ef4543
MatheusArruda2005 <163347306+MatheusArruda2005@users.noreply.github.com>
2025-04-11 09:25:02 -0300

    index.js

--- index.js
+++ index.js
@@ -26,3 +26,27 @@ console.log("Nome recuperado do localStorage:", nome);
 if (nome) {
   document.getElementById("boasVindas").textContent = `Bem-vindo, ${nome}!`;
 }
+
+  //Nome do projeto em cima da pagina do index
+window.addEventListener("DOMContentLoaded", function () {
+  const projeto = localStorage.getItem("tipoProjeto");
+  if (projeto) {
+    const boasVindas = document.getElementById("boasVindas");
+    const spanProjeto = document.createElement("span");
+    spanProjeto.textContent = projeto;
+    spanProjeto.style.color = "#007bff";
+    spanProjeto.style.fontWeight = "bold";
+    spanProjeto.style.textTransform = "capitalize";
+
+    const paragrafo = document.createElement("p");
+    paragrafo.appendChild(spanProjeto);
+    paragrafo.style.textAlign = "center";
+    paragrafo.style.marginTop = "10px";
+    paragrafo.style.fontSize = "2rem";
+    paragrafo.style.fontWeight = "bold";
+    paragrafo.style.fontFamily = "Arial, sans-serif";
+    paragrafo.style.textShadow = "2px 2px 5px rgba(0, 123, 255, 0.5)";
+
+    boasVindas.insertAdjacentElement("afterend", paragrafo);
+  }
+});
