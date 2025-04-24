b1c88c22121adf1f7de1218fb8312f5bc1990127
Lucas Salomão Boschiroli <152440853+lucasboschiroli@users.noreply.github.com>
2025-04-24 17:10:29 -0300

    Correção de configuração de conexão de portas

--- register.php
+++ register.php
@@ -6,7 +6,7 @@ $db_name = "devs_choice";
     $pass = "";
     $server = "localhost";
 
-    $conn = mysqli_connect($server, $user, $pass, $db_name, 3307);
+    $conn = mysqli_connect($server, $user, $pass, $db_name);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
