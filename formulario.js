5a2aa3e65d0eb8cc9fb3c51893be8315e0cec8e6
Lucas Salomão Boschiroli <152440853+lucasboschiroli@users.noreply.github.com>
2025-04-24 17:17:58 -0300

    Formulário atualizado

--- formulario.js
+++ formulario.js
@@ -2,7 +2,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
   event.preventDefault();
 
   const tipoProjeto = document.querySelector("input[type='text']").value.trim();
-  localStorage.setItem("tipoProjeto", tipoProjeto);
+  localStorage.setItem("nomeProjeto", nomeProjeto);
 
   window.location.href = "index.html";
 });
