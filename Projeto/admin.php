66e0c789aeabd96e058fa92abb59a9174e3fb5c3
João Vitor de Moraes Bringmann <149213160+JoaoBringmann@users.noreply.github.com>
2025-05-16 11:18:06 -0300

    admin.php

--- Projeto/admin.php
+++ Projeto/admin.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header("Location: adminLogin.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
